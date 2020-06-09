0b6e90a392876a6aa924723a0ef7f79a5dd0138e
João Pedro <jpedrodss@gmail.com>
2020-06-09 10:26:39 -0300

    Conta do motorista, pronta

---
+++ ContaMotorista.cs
@@ -0,0 +1,21 @@
+namespace Aula13Uber
+{
+    public class ContaMotorista : Motorista
+    {
+        public string agencia { get; set; }
+        public string conta { get; set; }
+
+        public string Cadastrar(){
+            return "Conta cadastrada";
+        }
+        public void Excluir(string resposta){
+            System.Console.WriteLine("Deseja excluir sua conta?");
+            resposta = System.Console.ReadLine();
+            if(resposta == "sim"){
+            System.Console.WriteLine($"Sua conta: {conta} foi excluída.");
+            }else{
+                System.Console.WriteLine($"Sua conta: {conta} não foi excluída.");
+            }
+        }
+    }
+}
